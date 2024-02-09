e08779d54e19a6f151a79b6a8b77b44c1b015334
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2024-02-09 01:19:04 +0000

    Added encrypt_password.py and user_data.csv

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+   5. Encrypting passwords
+   6. Check valid password
+'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    ''' Implement a hash_password function that expects one string
+                     argument name password and returns a salted, hashed
+                     password, which is a byte string.
+
+        Use the bcrypt package to perform the hashing (with hashpw).
+    '''
+    pass_encoded = password.encode()
+    pass_hashed = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
+
+    return pass_hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' Implement an is_valid function that expects 2 arguments and
+                    returns a boolean.
+    '''
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
